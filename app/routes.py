1b0ca2d05460c0671cc045110a3f522134a14e11
Anastasia <anastasia.butkevych@gmail.com>
2025-04-16 13:36:51 +0300

    Create app related files

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from .models import IngestDocs, ChatInputs
+
+
+app = FastAPI()
+
+
+@app.post("/ingest")
+async def ingest_document(doc_input: IngestDocs):
+    pass
+
+@app.post("/chat")
+async def chat(usr_input: ChatInputs):
+    pass
+
+
+@app.get("/")
+def index():
+    pass
