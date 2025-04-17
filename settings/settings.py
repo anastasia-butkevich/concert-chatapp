916677803d931432aeacb503f0a2ac834f7d314e
Anastasia <anastasia.butkevych@gmail.com>
2025-04-17 23:12:50 +0300

    Update settings

--- settings/settings.py
+++ settings/settings.py
@@ -7,3 +7,7 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+HOST = "0.0.0.0"
+PORT = 8000
+API_URL = "http://localhost:8000"
